062f15fa5f91dcedfa26ca9e91df9e0ca3db5d51
Markus Schratter <markus.schratter@v2c2.at>
2026-04-17 15:26:54 +0200

    add mounting of map_projector_info.yaml inside the docker

--- docker/lanelet2_generator.sh
+++ docker/lanelet2_generator.sh
@@ -58,6 +58,54 @@ INPUT_BASENAME="$(basename "${INPUT_PATH}")"
 
 docker build -f "${REPO_ROOT}/docker/Dockerfile" -t "${IMAGE_NAME}" "${REPO_ROOT}"
 
+EXTRA_ARGS=("$@")
+EXTRA_VOLUMES=()
+map_mount_id=0
+
+# If --map-projector-info points outside mounted input/output dirs, mount it.
+for ((i = 0; i < ${#EXTRA_ARGS[@]}; i++)); do
+  arg="${EXTRA_ARGS[$i]}"
+  map_path=""
+  inline=0
+
+  if [[ "${arg}" == "--map-projector-info" ]]; then
+    if (( i + 1 < ${#EXTRA_ARGS[@]} )); then
+      map_path="${EXTRA_ARGS[$((i + 1))]}"
+    fi
+  elif [[ "${arg}" == --map-projector-info=* ]]; then
+    map_path="${arg#--map-projector-info=}"
+    inline=1
+  fi
+
+  if [[ -z "${map_path}" || "${map_path}" != /* ]]; then
+    continue
+  fi
+  if [[ ! -f "${map_path}" ]]; then
+    continue
+  fi
+
+  map_dir="$(dirname "${map_path}")"
+  map_base="$(basename "${map_path}")"
+  container_map_path=""
+
+  if [[ "${map_dir}" == "${INPUT_DIR}" ]]; then
+    container_map_path="/input/${map_base}"
+  elif [[ "${map_dir}" == "${OUTPUT_DIR_ABS}" ]]; then
+    container_map_path="/output/${map_base}"
+  else
+    mount_point="/map_projector_${map_mount_id}"
+    map_mount_id=$((map_mount_id + 1))
+    EXTRA_VOLUMES+=("-v" "${map_dir}:${mount_point}:ro")
+    container_map_path="${mount_point}/${map_base}"
+  fi
+
+  if (( inline == 1 )); then
+    EXTRA_ARGS[$i]="--map-projector-info=${container_map_path}"
+  else
+    EXTRA_ARGS[$((i + 1))]="${container_map_path}"
+  fi
+done
+
 DOCKER_ARGS=(
   --rm
   -w /app
@@ -88,5 +136,4 @@ else
   )
 fi
 
-docker run "${DOCKER_ARGS[@]}" \
-    "$@"
+docker run "${EXTRA_VOLUMES[@]}" "${DOCKER_ARGS[@]}" "${EXTRA_ARGS[@]}"
